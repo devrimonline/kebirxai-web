a586f47f88ec2c8397cd16fdaad9b84510ca731a
devrimonline <devrimonline@gmail.com>
2025-09-27 20:06:42 +0300

    kebirxwww

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './KebirXAI'; // Dosya adı tam olarak KebirXAI olmalı
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
